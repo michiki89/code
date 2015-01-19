b1231590d3ba3f6bafd0c1da83063d543586adf5
Michael Kircher <michael.kircher@student.kit.edu>
2015-01-19 10:02:50 +0100

    Wrote new LS Gauss Algorithm, which finally filters in time domain

--- lsGauss_timeDomain.m
+++ lsGauss_timeDomain.m
@@ -32,7 +32,43 @@ xspec = cpsd(RRiSig,respSig,N_window,N_overlap,N_fft,'twosided');   % cross spec
 
 %% LS sampling point extraction
 [~,Nmax] = min(abs(f-fmax));        % find sample, where fmax in cross spectrum
-% [~,Nmin_left] = findpeaks(
+
+% Find minima next to main lobe (assumption: zeropadding and welch averaging is large enough
+% that PSD is smoothed good enough for robust minima detection)
+[~,Nmin_left] = findpeaks(-abs(xspec(1:Nmax,1)),1,'last');   % Find minimum to the left of the main peak
+[~,Nmin_right] = findpeaks(-abs(xspec(N_max+1:end/2,1)),1,'first');   % FInd minimum to the right of the main peak
+
+% Check if Nmin_right larger than Nmin_left
+if Nmin_right < Nmin_right
+    error('LS sampling point extraction failed');
+end
+
+%% LS estimation
+% the parameters of a gaussian bell are estimated:
+% y(n) = B*exp(-A*n) --> y_tilde(n) = ln(y(n)) = ln(B) - A*n = C - A*n
+
+% setting up the linear model for the LS estimation
+y_tilde = ln(abs(xspec(Nmin_left:Nmin_right,1)));
+freq    = f(Nmin_left:Nmin_right,1);
+X = [-freq,ones(length(freq),1)];
+
+% Actual LS estimation
+lambda = inv(X'*X)*X'*y_tilde;
+
+% Parameter extraction
+B = exp(lambda(2,1));
+A = lambda(1,1);
+
+% get spectral filter curve
+gaussFilt = B*exp(-A*f(1:end/2,1));
+gaussFilt = [gaussFilt;0;flip(gaussFilt(1:end-1,1))];
+
+
+
+
+
+
+
 
 
 
